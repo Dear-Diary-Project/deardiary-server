26f2b99a5727bbf1680185c30c8cf1da2a822fdf
Pruthvi <pruthvishetty5656@gmail.com>
2020-10-23 05:51:50 +0530

    Index file added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log("Dear Diary Begin!")
+import { createConnection } from "typeorm";
+import { Entry } from "./entities/Entry";
+import { User } from "./entities/User";
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+const main = async () => {
+    const conn  = await createConnection({
+        type: 'postgres',
+        database: 'deardiary',
+        username: 'pruthvi',
+        logging:true,
+        synchronize: true,
+        entities: [Entry,User],
+    });
+
+    const app = express();
+
+    const apoloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    })
+
+    apoloServer.applyMiddleware({ app })
+
+    app.listen(4000, () => {
+        console.log('Server Started on localhost:4000')
+    })
+    //console.log(conn);
+
+}
+
+main()
