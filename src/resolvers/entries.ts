ae6f761641d0a4dbbd64c324cf0d1e4499380708
Pruthvi <pruthvishetty5656@gmail.com>
2020-10-23 05:50:18 +0530

    Diary Entry resolver added
    Full CRUD Added with documentation

---
+++ src/resolvers/entries.ts
@@ -0,0 +1,45 @@
+import { Resolver,Query, Arg, Mutation} from 'type-graphql';
+import {Entry} from "../entities/Entry";
+
+
+@Resolver()
+export class EntryResolver{
+    @Query(() => [Entry])
+    async entries(): Promise<Entry[]> {
+        return Entry.find();
+    }
+
+    @Query(() => Entry, {nullable:true})
+    entry(@Arg("id") id: number): Promise<Entry | undefined> {
+        return Entry.findOne(id);
+    }
+
+    @Mutation(() => Entry)
+    async createEntry(@Arg("title") title: string): Promise<Entry>{
+        return Entry.create({title}).save();
+    }
+
+    @Mutation(() => Entry, {nullable:true})
+    async updateEntry(
+        @Arg("id") id: number,
+        @Arg("title" , () => String, {nullable:true}) title:string
+    ): Promise<Entry|null> {
+        const entry = await Entry.findOne(id);
+        if(!entry){
+            return null;
+        }
+        if (typeof title !== "undefined"){
+            await Entry.update({ id }, {title});
+        }
+        return entry;
+    }
+
+    @Mutation(() => Boolean)
+    async deleteEntry(
+        @Arg("id") id:number,
+    ): Promise<boolean> {
+        await Entry.delete(id)
+        return true;
+    }
+    
+}
