6ffe4adbeff4ca8f02c616a3e85ae70f6a7adfd5
Pruthvi <pruthvishetty5656@gmail.com>
2020-10-23 05:49:17 +0530

    Entry Entity added
    Diary Entity added

---
+++ src/entities/Entry.ts
@@ -0,0 +1,23 @@
+import {Field, ObjectType} from 'type-graphql';
+import {Entity,Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, BaseEntity} from 'typeorm';
+
+@ObjectType()
+@Entity()
+export class Entry extends BaseEntity{
+    @Field()
+    @PrimaryGeneratedColumn()
+    id!:number;
+
+    @Field(() => String)
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @Field(() => String)
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @Field()
+    @Column({type: "text"})
+    title!:String;
+
+}
