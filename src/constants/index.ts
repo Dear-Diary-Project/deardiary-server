11fb78e02101f7b9d71c63e7ecfd2a56e7806f77
ZeffK <54438151+ZeffK@users.noreply.github.com>
2020-12-02 17:39:46 +0530

    Just a test commit to push new branch to heroku

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 export const SERVER_PORT = process.env.PORT || 3000;
 export const __prod__ = process.env.NODE_ENV === 'production';
-export const DATABASE_URL = process.env.DATABASE_URL || 'postgres://postgres:admin@localhost:5432/deardiary';
+export const DATABASE_URL = process.env.DATABASE_URL || 'postgres://ywviokpwxchnhs:cf1f254ac2d7b7c4ed72bfe962c2fdb1c9d8168b46e3a0065418ea9f4e1a7cd7@ec2-3-231-48-230.compute-1.amazonaws.com:5432/dbeuetq68n3mft' ;
